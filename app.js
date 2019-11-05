6d3129990673f102f54bc0ddfcf76ca894608fb7
ayogatot <azerino25@gmail.com>
2019-11-05 16:37:52 +0700

    Adding  some package & preparing html-css files

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
 require("dotenv").config();
 
 const app = require("express")();
+const bodyParser = require("body-parser");
 const PORT = process.env.PORT || 3000;
 
+const msgRoute = require("./routes/msg");
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use("/api/v1/messages", msgRoute);
+
 app.get("/", (req, res) => {
-  res.send("<h1> HELLO WORLD <h1>");
+  res.sendFile(__dirname + "/public/views/index.html");
 });
 
 app.listen(PORT, () => {
