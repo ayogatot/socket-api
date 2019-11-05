a89c8a402af169bbbd0fac0fb4d9bb8623bdd5e2
ayogatot <azerino25@gmail.com>
2019-11-05 17:28:31 +0700

    Fixing structure and code

--- app.js
+++ app.js
@@ -1,20 +1,47 @@
 require("dotenv").config();
 
 const app = require("express")();
+const http = require("http").Server(app);
+const io = require("socket.io")(http);
 const bodyParser = require("body-parser");
 const PORT = process.env.PORT || 3000;
 
-const msgRoute = require("./routes/msg");
+const data = require("./datas/msg");
+const { sendResponse } = require("./helpers/index");
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use("/api/v1/messages", msgRoute);
-
-app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/public/views/index.html");
+io.on("connection", socket => {
+  socket.on("message", msg => {
+    io.emit("message", msg);
+  });
 });
 
-app.listen(PORT, () => {
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.get("/v1/messages", (req, res) => {
+  try {
+    sendResponse(res, 200, false, null, data);
+  } catch (error) {
+    sendResponse(res, 500, true, error, []);
+  }
+});
+
+app.post("/v1/messages", (req, res) => {
+  try {
+    const { message } = req.body;
+    data.message.push(message);
+
+    io.emit("message", message);
+    sendResponse(res, 200, false, null, data);
+  } catch (error) {
+    sendResponse(res, 500, true, error, []);
+  }
+});
+
+http.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
 });
